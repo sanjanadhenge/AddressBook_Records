b32f99f7ac7ab7a93c9e7d24cc3fa25e6421fd70
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-04-25 15:25:33 +0530

    [sanjana] Add :Retrive Data by city and state

--- AddressBookSysytem/Operation.cs
+++ AddressBookSysytem/Operation.cs
@@ -16,7 +16,7 @@ namespace AddressBookSysytem
         public void GetAllRecords()
         {
 
-          
+
             try
             {
                 using (this.connection)
@@ -41,7 +41,7 @@ namespace AddressBookSysytem
                             contact.Email = read.GetString(8);
                             contact.StartDate = read.GetDateTime(9);
 
-                            Console.WriteLine(contact.ID + "\n" + contact.FirstName + "\n" + contact.LastName + "\n" + contact.Address + "\n" + contact.City + "\n" + contact.State + "\n" + contact.zip + "\n" + contact.PhoneNumber + "\n" + contact.Email + "\n"+ contact.StartDate);
+                            Console.WriteLine(contact.ID + "\n" + contact.FirstName + "\n" + contact.LastName + "\n" + contact.Address + "\n" + contact.City + "\n" + contact.State + "\n" + contact.zip + "\n" + contact.PhoneNumber + "\n" + contact.Email + "\n" + contact.StartDate);
                         }
 
                     }
@@ -98,7 +98,7 @@ namespace AddressBookSysytem
             {
                 throw new Exception(ex.Message);
             }
-            
+
         }
         public void AddEmployee(Contacts contact)
         {
@@ -176,5 +176,92 @@ namespace AddressBookSysytem
 
             }
         }
+        public void RetriveDataByCity(string city)
+        {
+            try
+            {
+                using (this.connection)
+                {
+                    string query = @"select * from  RecordsDB";
+                    SqlCommand command = new SqlCommand(query, this.connection);
+                    command.CommandType = System.Data.CommandType.Text;
+                    this.connection.Open();
+                    SqlDataReader read = command.ExecuteReader();
+                    if (read.HasRows)
+                    {
+                        while (read.Read())
+                        {
+                            contact.ID = read.GetInt32(0);
+                            contact.FirstName = read.GetString(1);
+                            contact.LastName = read.GetString(2);
+                            contact.Address = read.GetString(3);
+                            contact.City = read.GetString(4);
+                            contact.State = read.GetString(5);
+                            contact.zip = read.GetString(6);
+                            contact.PhoneNumber = read.GetString(7);
+                            contact.Email = read.GetString(8);
+                            contact.StartDate = read.GetDateTime(9);
+                            if (contact.City.Equals(city))
+                                Console.WriteLine(contact.ID + "\n" + contact.FirstName + "\n" + contact.LastName + "\n" + contact.Address + "\n" + contact.City + "\n" + contact.State + "\n" + contact.zip + "\n" + contact.PhoneNumber + "\n" + contact.Email + "\n" + contact.StartDate);
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("No Records avaible");
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        public void RetriveDataByState(string State)
+        {
+            try
+            {
+                using (this.connection)
+                {
+                    string query = @"select * from  RecordsDB";
+                    SqlCommand command = new SqlCommand(query, this.connection);
+                    command.CommandType = System.Data.CommandType.Text;
+                    this.connection.Open();
+                    SqlDataReader read = command.ExecuteReader();
+                    if (read.HasRows)
+                    {
+                        while (read.Read())
+                        {
+                            contact.ID = read.GetInt32(0);
+                            contact.FirstName = read.GetString(1);
+                            contact.LastName = read.GetString(2);
+                            contact.Address = read.GetString(3);
+                            contact.City = read.GetString(4);
+                            contact.State = read.GetString(5);
+                            contact.zip = read.GetString(6);
+                            contact.PhoneNumber = read.GetString(7);
+                            contact.Email = read.GetString(8);
+                            contact.StartDate = read.GetDateTime(9);
+                            if (contact.City.Equals(State))
+                                Console.WriteLine(contact.ID + "\n" + contact.FirstName + "\n" + contact.LastName + "\n" + contact.Address + "\n" + contact.City + "\n" + contact.State + "\n" + contact.zip + "\n" + contact.PhoneNumber + "\n" + contact.Email + "\n" + contact.StartDate);
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("No Records avaible");
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
     }
 }
+
+    
+
